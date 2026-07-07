6aade08c8bf077381c4b72994fe7ab839018c800
Samir Zade <samirzadeofficial43@gmail.com>
2026-07-07 21:46:07 +0530

    feat(retrieval): implement hybrid retrieval engine

--- ecip_core/retrieval/__init__.py
+++ ecip_core/retrieval/__init__.py
@@ -1,3 +1,4 @@
 from ecip_core.retrieval.semantic_search import SemanticSearch
+from ecip_core.retrieval.hybrid_retrieval import HybridRetrieval
 
-__all__ = ["SemanticSearch"]
+__all__ = ["SemanticSearch", "HybridRetrieval"]
--- ecip_core/retrieval/models/__init__.py
+++ ecip_core/retrieval/models/__init__.py
@@ -1,4 +1,5 @@
 from ecip_core.retrieval.models.search_result import SearchResult
 from ecip_core.retrieval.models.metadata_result import MetadataResult
+from ecip_core.retrieval.models.hybrid_result import HybridResult
 
-__all__ = ["SearchResult", "MetadataResult"]
+__all__ = ["SearchResult", "MetadataResult", "HybridResult"]
